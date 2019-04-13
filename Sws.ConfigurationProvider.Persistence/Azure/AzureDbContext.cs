1d326e4603e5c0abc8aea282a50aab8a762405fa
Karas, Petr <petakaras@seznam.cz>
2019-04-13 22:16:24 +0200

    Hardcoding connection string

--- Sws.ConfigurationProvider.Persistence/Azure/AzureDbContext.cs
+++ Sws.ConfigurationProvider.Persistence/Azure/AzureDbContext.cs
@@ -1,9 +1,7 @@
-﻿using System.Collections.Generic;
-using Microsoft.EntityFrameworkCore;
-using Sws.ConfigurationProvider.Adapters.System;
-using Sws.ConfigurationProvider.Core;
+﻿using Microsoft.EntityFrameworkCore;
 using Sws.ConfigurationProvider.Core.Entities.Azure;
 using Sws.ConfigurationProvider.Persistence.Azure.EntityMapping;
+using System.Collections.Generic;
 
 namespace Sws.ConfigurationProvider.Persistence.Azure
 {
@@ -11,14 +9,15 @@ namespace Sws.ConfigurationProvider.Persistence.Azure
     {
         public AzureDbContext()
         {
-            
+
         }
-        private static readonly IEnumerable<IEntityTypeMap> Mappings = new []{new AzureScaleSetMap(), };
+        private static readonly IEnumerable<IEntityTypeMap> Mappings = new[] { new AzureScaleSetMap(), };
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
-            optionsBuilder.UseSqlServer(SystemAdapter.GetMachineEnvVariable(Constants.ConnectionStringName));
+            //optionsBuilder.UseSqlServer(SystemAdapter.GetMachineEnvVariable(Constants.ConnectionStringName));
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Database=AzureData;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trusted_Connection = True;");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
